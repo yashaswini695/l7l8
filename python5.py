fa95116d1a544dbc1e8170f5061b149a91efd2c2
yashaswini695 <yashaswini.b@s.amity.edu>
2025-02-28 19:00:39 +0530

    https://github.com/yashaswini695/l7l8

---
+++ python5.py
@@ -0,0 +1,10 @@
+#create a list of numbers
+#sort it in descending order and reverse the sorted list and print it.
+
+list = [9,8,7,6,5,4]
+list.sort()
+print(list)
+
+list = [4,5,6,7,8,9]
+list.reverse()
+print(list)
