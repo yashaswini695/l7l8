adfc184c3059766120474496e3f543a2c5d717ac
yashaswini695 <yashaswini.b@s.amity.edu>
2025-03-04 11:09:41 +0530

    c programming

---
+++ c4.c
@@ -0,0 +1,12 @@
+#include<stdio>
+int main()
+{
+	int n:
+		printf("enter the positive interger: ");
+	scanf("%d",7n);
+	printf("even number from 1 to 20%d:\n",n);
+	for(int i=2;i<=n;i+=2)
+	{
+		printf("%d",i);
+	}return 0:
+}
