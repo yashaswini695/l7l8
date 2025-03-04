d32ce1ec6ac5df8b6474164b8d0b8b848746ab9a
yashaswini695 <yashaswini.b@s.amity.edu>
2025-03-04 12:17:24 +0530

    primenumber

---
+++ pythoncode.py
@@ -0,0 +1,18 @@
+
+
+num = 29
+
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
